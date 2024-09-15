f40071f0074ef709908f540b7145640d69fb5dcd
Bishnusilwal1 <bishnusilwal999@gmail.com>
2024-09-15 18:30:09 +0545

    feat: create post code page

---
+++ src/types/draft-js.d.ts
@@ -0,0 +1,9 @@
+// types/draft-js.d.ts
+declare module 'draft-js' {
+    export class EditorState {
+      static createEmpty(): EditorState;
+      // Add any other required types or methods here
+    }
+    // Export any other types or methods you need from draft-js
+  }
+  
